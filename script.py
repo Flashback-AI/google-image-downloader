6db26ee5e25c01ee536793db5fc2e4c8a73088d1
Tanmay Vaish <55585498+LEON6156SCOTT@users.noreply.github.com>
2021-01-13 02:57:46 +0530

    Added Selenium to cover more photos
    We get features like opening the browser and scrolling through the pages.

--- script.py
+++ script.py
@@ -2,7 +2,8 @@ import os
 import json
 import requests # for get requests
 from bs4 import BeautifulSoup
-
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 
 base_url = 'https://www.google.com/search?site=&tbm=isch&source=hp&biw=1873&bih=990&'
 
@@ -15,6 +16,7 @@ user_agent = {
     'Connection': 'keep-alive',
 }
 folder = "Images"
+
 
 
 def main():
@@ -32,13 +34,34 @@ def download():
 
     print("Searching...")
 
+    options = Options()
+    options.binary_location = 'C:/Program Files (x86)/BraveSoftware/Brave-Browser/Application/brave.exe'
+    driver_path = 'C:/Program Files (x86)/BraveSoftware/Brave-Browser/Application/chromedriver.exe'
+    driver = webdriver.Chrome(options = options, executable_path = driver_path)
+
     search_url = base_url + 'q=' + keyword
     print(search_url)
 
-    response = requests.get(search_url,user_agent)
-    markup = response.text
+#   response = requests.get(search_url,user_agent)
+#   markup = response.text # idhar gadbad ho sakti hai
 
-    crawler = BeautifulSoup(markup,'html.parser')
+    driver.get(search_url)
+
+    i=0
+
+    while i<7:  
+	    #for scrolling page
+        driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
+    
+    try:
+		#for clicking show more results button
+        driver.find_element_by_xpath("/html/body/div[2]/c-wiz/div[3]/div[1]/div/div/div/div/div[5]/input").click()
+    except Exception as e:
+        pass
+    time.sleep(5)
+    i+=1
+
+    crawler = BeautifulSoup(driver.page_source,'html.parser')
     results = crawler.find_all(name = 'img',attrs = {'class':'t0fcAb'},limit=number)
 
     links = []
@@ -46,11 +69,11 @@ def download():
     for i in results:
         
         img_link = i['src']
-        links.append(img_link)
+        links.append(img_link) # Storing all the image links to a list 
 
     if len(links) == 0:
         print("ERROR, No Images Found")
-        exit(0)
+        exit(0) # Exception Handling
 
     print(f'found {len(links)} images')
     print('downloading...')
@@ -62,7 +85,8 @@ def download():
         imagename = folder + '/' + keyword + str(i+1) + '.jpg'
         with open(imagename, 'wb') as file:
             file.write(response.content)
-
+    
+    driver.close()
     print('Done')
 
 
